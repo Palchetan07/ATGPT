e09386b2221bbe332257a90360db37b5e88a60bc
Chetan <palchetan187@gmail.com>
2023-09-13 00:06:49 +0530

    New Button added!

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AT GPT BOT',
+      title: 'AT GPT BOt',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.cyan,
